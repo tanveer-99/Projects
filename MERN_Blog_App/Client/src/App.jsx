4ee2c097f0dfadf1e4d13bb1cbb488c529910088
tanveer-99 <tanvir3512@gmail.com>
2024-10-22 12:51:10 +0100

    fixed the tailwind problem and created the pages

--- MERN_Blog_App/Client/src/App.jsx
+++ MERN_Blog_App/Client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='Text-3xl text-red-400'>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ MERN_Blog_App/Client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-red-400'>hello world!</h1><br />
+    </div>
+  )
+}
+
+export default Home
