67c2eb9b70285c4a87050954ad6e79218ffc4e17
tanveer-99 <tanvir3512@gmail.com>
2024-10-28 01:41:52 +0000

    connected to the database

--- MERN_Blog_App/Server/index.js
+++ MERN_Blog_App/Server/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
+
+// connect to mongoDB
+mongoose.connect(process.env.MONGO)
+.then(()=> {
+    console.log("connected to DB")
+})
+.catch((err)=> {
+    console.log(err)
+})
+
 
 app.listen(3000, ()=> {
     console.log("listening to port 3000")
