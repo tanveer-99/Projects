57ccc95e5d04d2a69b23018d1afc8d3b6ee0a7bf
tanveer-99 <tanvir3512@gmail.com>
2024-11-05 00:44:39 +0000

    sign in api

--- MERN_Blog_App/Server/controllers/auth.controller.js
+++ MERN_Blog_App/Server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js"
 import bcyrptjs from 'bcryptjs'
+import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next)=> {
     const { username, email, password } = req.body
@@ -13,6 +15,25 @@ export const signup = async (req, res, next)=> {
     try {
         const user = await newUser.save();
         res.status(200).send(user);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signin = async(req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser) {
+            return next(errorHandler(404,'Invalid Credentials!'))
+        }
+        const validPassword = bcyrptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(400,'Invalid Credentials!'))
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_TOKEN);
+        const { password: pass, ...rest} = validUser._doc
+        res.status(200).cookie('access_token', token, {httpOnly:true}).json(rest)
     } catch (error) {
         next(error)
     }
