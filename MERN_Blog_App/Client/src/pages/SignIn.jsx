da3aa03d1bb7273653216fcaccc9481faf90d0a0
tanveer-99 <tanvir3512@gmail.com>
2024-11-06 13:24:19 +0000

    add redux toolkit

--- MERN_Blog_App/Client/src/pages/SignIn.jsx
+++ MERN_Blog_App/Client/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+import {useDispatch, useSelector} from 'react-redux'
+
 
 const SignIn = () => {
   const [formData, setFormdata] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const {loading, error: errorMessage} = useSelector(state =>  state.user)
 
   const handleChange = e => {
     setFormdata({...formData, [e.target.id]:e.target.value.trim()})
@@ -16,11 +19,10 @@ const SignIn = () => {
   const handleSubmit = async (e)=> {
     e.preventDefault()
     if(!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all the fields!'); 
+      return dispatch(signInFailure('Please fill out all the fields!')); 
     }
     try {
-      setLoading(true)
-      setErrorMessage(null)
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -30,15 +32,14 @@ const SignIn = () => {
       })
       const data = await res.json()
        if(data.success == false) {
-        setErrorMessage(data.message)
+        dispatch(signInFailure(data.message))
        } 
-       setLoading(false)
        if (res.ok) {
+        dispatch(signInSuccess(data))
         navigate('/')
        }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false)
+      dispatch(signInFailure(error.message))
     }
   }
   console.log(formData)
