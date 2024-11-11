4205344b9f1f19db02e9a9e12bab764504d2b001
tanveer-99 <tanvir3512@gmail.com>
2024-11-11 00:09:34 +0000

    add dashbard to private route

--- MERN_Blog_App/Client/src/App.jsx
+++ MERN_Blog_App/Client/src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
 import FooterComponent from './components/FooterComponent'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -18,7 +19,9 @@ const App = () => {
         <Route path='/about' element={<About />}></Route>
         <Route path='/sign-in' element={<SignIn />}></Route>
         <Route path='/sign-up' element={<SignUp />}></Route>
-        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route element={<PrivateRoute></PrivateRoute>}>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+        </Route>
         <Route path='/projects' element={<Projects />}></Route>
       </Routes>
       <FooterComponent></FooterComponent>
