51ad425d3d97c044a43aeb9307efe6ce865e16fa
tanveer-99 <tanvir3512@gmail.com>
2024-11-20 18:20:20 +0000

    delete user account functionality

--- MERN_Blog_App/Client/src/components/DashProfile.jsx
+++ MERN_Blog_App/Client/src/components/DashProfile.jsx
@@ -1,13 +1,14 @@
-import { Alert, Button, TextInput } from 'flowbite-react'
+import { Alert, Button, Modal, TextInput } from 'flowbite-react'
 import React, { useEffect, useRef, useState } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { app } from '../firebase.js'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { HiOutlineExclamationCircle} from 'react-icons/hi'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
-import { updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice.js';
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice.js';
 const DashProfile = () => {
-    const {currentUser} = useSelector(state => state.user)
+    const {currentUser, error} = useSelector(state => state.user)
     const [imageFile, setImageFile] = useState(null)
     const [imageFileUrl, setImageFileUrl]= useState(null)
     const [uploadProgess, setUploadProgress] = useState(null)
@@ -15,6 +16,7 @@ const DashProfile = () => {
     const [imageFileUploading, setImageFileUploading] = useState(false)
     const [updateUserSuccess , setUpdateUserSuccess] = useState(null)
     const [updateUserError, setUpdateUserError] = useState(null)
+    const [showModal, setShowModal] = useState(false)
     const filePickerRef = useRef();
     const [formData, setFormData] = useState({})
     const dispatch = useDispatch()
@@ -105,6 +107,24 @@ const DashProfile = () => {
             setUpdateUserError(error.message)
         }
     }
+    const handleDeleteUser = async()=> {
+        setShowModal(false)
+        try {
+            dispatch(deleteUserStart())
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: 'DELETE'
+            })
+            const data = await res.json()
+            if(!res.ok) {
+                dispatch(deleteUserFailure(data.message))
+            }
+            else {
+                dispatch(deleteUserSuccess(data))
+            }
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message))
+        }
+    }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -160,11 +180,11 @@ const DashProfile = () => {
                 placeholder='password'
                 onChange={handleChange}
             ></TextInput>
-            <Button type='submit' gradientDuoTone='purpleToBlue' outline>Submit</Button>
+            <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update User</Button>
 
         </form>
         <div className="text-red-500 flex justify-between mt-5">
-            <span className='cursor-pointer'>Delete Account</span>
+            <span onClick={()=> setShowModal(true)} className='cursor-pointer'>Delete Account</span>
             <span className='cursor-pointer'>Sign Out</span>
         </div>
         {updateUserSuccess && 
@@ -173,6 +193,22 @@ const DashProfile = () => {
         {updateUserError && 
             <Alert color='failure' className='mt-5'>{updateUserError}</Alert>
         }
+        {error &&
+            <Alert color='failure' className='mt-5'>{updateUserError}</Alert> 
+        }
+        <Modal show={showModal} onClose={()=> setShowModal(false)} popup size='md'>
+            <Modal.Header></Modal.Header>
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'></HiOutlineExclamationCircle>
+                    <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Are you sure you want to delete your account?</h3>
+                    <div className='flex justify-center gap-4'>
+                        <Button color='failure' onClick={handleDeleteUser}>Yes, I'm sure.</Button>
+                        <Button color='gray' onClick={()=>setShowModal(false)}>NO, cancel.</Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
     </div>
   )
 }
--- MERN_Blog_App/Client/src/redux/user/userSlice.js
+++ MERN_Blog_App/Client/src/redux/user/userSlice.js
@@ -35,9 +35,22 @@ const userSlice = createSlice({
         updateFailure: (state, action) => {
             state.loading = false
             state.error = action.payload
-        }
+        },
+        deleteUserStart: (state) => {
+            state.loading = true
+            state.error = null
+        },
+        deleteUserSuccess: (state) => {
+            state.loading = false
+            state.error = null
+            state.currentUser = null
+        },
+        deleteUserFailure: (state, action) => {
+            state.loading = false
+            state.error = action.payload
+        },
     }
 })
 
-export const {signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure} = userSlice.actions
+export const {signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure} = userSlice.actions
 export default userSlice.reducer
