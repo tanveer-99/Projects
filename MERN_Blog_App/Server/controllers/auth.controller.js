47e8ba23de8e4de6de033e45b040a861661b0162
tanveer-99 <tanvir3512@gmail.com>
2024-11-20 19:05:17 +0000

    add admin functionality to the user

--- MERN_Blog_App/Server/controllers/auth.controller.js
+++ MERN_Blog_App/Server/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const signin = async(req, res, next) => {
         if(!validPassword) {
             return next(errorHandler(400,'Invalid Credentials!'))
         }
-        const token = jwt.sign({id:validUser._id},process.env.JWT_TOKEN);
+        const token = jwt.sign({id:validUser._id, isAdmin:validUser.isAdmin},process.env.JWT_TOKEN);
         const { password: pass, ...rest} = validUser._doc
         res.status(200).cookie('access_token', token, {httpOnly:true}).json(rest)
     } catch (error) {
@@ -44,7 +44,7 @@ export const google = async (req, res, next)=> {
     try {
         const user = await User.findOne({email})
         if(user) {
-            const token = jwt.sign({id:user._id}, process.env.JWT_TOKEN)
+            const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT_TOKEN)
             const {password, ...rest} = user._doc
             res.status(200).cookie('access_token', token, {
                 httpOnly: true
@@ -61,7 +61,7 @@ export const google = async (req, res, next)=> {
                 profilePicture: googlePhotoURL 
             })
             await newUser.save()
-            const token = jwt.sign({id:user._id}, process.env.JWT_TOKEN)
+            const token = jwt.sign({id:user._id,isAdmin:newUser.isAdmin}, process.env.JWT_TOKEN)
             const {password, ...rest} = newUser._doc
             res.status(200).cookie('access_token', token, {
                 httpOnly: true
