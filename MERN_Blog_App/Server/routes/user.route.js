f2da9e71f600716e038c4b386afa537844325c5b
tanveer-99 <tanvir3512@gmail.com>
2024-11-20 17:49:30 +0000

    delete user api

--- MERN_Blog_App/Server/routes/user.route.js
+++ MERN_Blog_App/Server/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { test, updateUser } from '../controllers/user.controller.js'
+import { test, updateUser, deleteUser } from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 const router = Router()
 
 router.get('/', test)
 router.put('/update/:userId', verifyToken, updateUser)
+router.delete('/delete/:userId', verifyToken, deleteUser)
 
 export default router
